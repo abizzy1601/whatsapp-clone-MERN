71811445bf10a5a2686f559a686a1dac295983ef
abizzy1601 <70805279+abizzy1601@users.noreply.github.com>
2020-12-01 02:19:25 +0100

    Update Chat.js

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-//import React from "react";
+import React from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
